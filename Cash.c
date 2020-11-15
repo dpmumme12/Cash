cc10efaad73a33af9b7fdf49352d2f3058f56828
Doug Mumme <dougmumme@gmail.com>
2020-11-14 22:54:52 -0600

    First commit

---
+++ Cash.c
@@ -0,0 +1,35 @@
+#include <math.h>
+//#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    float k;
+    int c = 0;
+    int r = 0;
+    do
+    {
+        printf("Change owed: ");
+        scanf("%d", &k);
+    }
+    while (k <= 0);
+    int cents = round(k * 100);
+    for (c = r; cents >= 25; r++) // quarter count
+    {
+        cents = cents - 25;
+    }
+    for (c = r ; cents >= 10; r++) // dime count
+    {
+        cents = cents - 10;
+    }
+    for (c = r; cents >= 5; r++) // nickel count
+    {
+        cents = cents - 5;
+    }
+    for (c = r; cents >= 1; r++) // penny count
+    {
+        cents = cents - 1;
+    }
+    printf("%d\n", r);
+    
+}
